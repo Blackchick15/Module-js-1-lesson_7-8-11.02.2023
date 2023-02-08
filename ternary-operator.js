4a807d1ea8fe502ac844a566f7fca0134bbd0167
Oksana Petryshyn <opetryshyn2@gmail.com>
2023-02-08 22:44:15 +0200

    коміт

---
+++ ternary-operator.js
@@ -0,0 +1,14 @@
+// - Тернарний оператор.
+
+let balance = 1000;
+let message;
+
+// if (balance >= 0) {
+//     message = "Позитивний баланс";
+// } else {
+//     message = "Негативний баланс";
+// }
+
+
+
+// console.log(message);
